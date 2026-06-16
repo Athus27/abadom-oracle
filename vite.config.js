59a49e297d34175bb7524ce8e8035ce4f59268a2
Athus27 <athussilva27@gmail.com>
2026-06-16 11:09:57 -0300

    fix: rename base url

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import path from "path" // Módulo nativo do Node para lidar com caminhos de arquivos
-import { fileURLToPath } from "url"
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import tailwindcss from "@tailwindcss/vite"
+import path from "path"; // Módulo nativo do Node para lidar com caminhos de arquivos
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/Oraculo-da-Morte/',
-  resolve: {
-    alias: {
-      // Aqui você define que o "@" aponta para a pasta "src"
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+	plugins: [react(), tailwindcss()],
+	base: "/abadom-oracle/",
+	resolve: {
+		alias: {
+			// Aqui você define que o "@" aponta para a pasta "src"
+			"@": path.resolve(__dirname, "./src")
+		}
+	}
+});
